b3a072893a853f5d4c024d768537a40f67b8215b
VICTORIA JOANA KRIGER CONTI <49439065@est.ort.edu.ar>
2026-04-30 09:05:52 -0300

    conecta axios en detalles, falta mostrarlo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,6 @@ function App() {
     return (
         <div className="container">
             <SearchBar setResults={setResults} />
-
             {selectedMovie ? (
                 <>
                     <button onClick={() => setSelectedMovie(null)}>
--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -2,24 +2,49 @@ import React, { useState, useEffect } from 'react';
 import api from '../api';
 
 const MovieDetail = ({ movie }) => {
+  const [errore, setErrore] = useState(false);
+  const [resultados, setResultados] = useState(null);
+  useEffect(async () => {    
+      try {
+        setErrore(false);
+        const response = await api.get("/", {
+          params: {
+            apikey: "8fb39e07",
+            i: movie.imdbID
+          }
+        });
+        console.log(response.data)
+        if (response.data === null) {
+          setErrore(true);
+          setResultados(null);
+        } else {
+          setResultados(response.data);
+        }
+
+      } catch (err) {
+        setErrore(true);
+      }    
+  }, [])
+
+
   return (
     <>
       <div className="card-detalles">
         <img
-          src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/150"}
-          alt={movie.Title}
+          src={resultados?.Poster !== "N/A" ? resultados?.Poster : "https://via.placeholder.com/150"}
+          alt={resultados?.Title}
         />
-        <p>Título: {movie.Title}</p>
-        <p>Año: {movie.Year}</p>
-        <p>Tipo: {movie.Type}</p>
-        <p>Género: {movie.Genre}</p>
-        <p>Director: {movie.Director}</p>
-        <p>Actores principales: {movie.Actors}</p>
-        <p>Sinopsis: {movie.Plot}</p>
-        <p>Duración: {movie.Runtime}</p>
-        <p>Idioma: {movie.Language}</p>
-        <p>País: {movie.Country}</p>
-        <p>Puntaje IMDb: {movie.imdbRating}</p>
+        <p>Título: {resultados?.Title}</p>
+        <p>Año: {resultados?.Year}</p>
+        <p>Tipo: {resultados?.Type}</p>
+        <p>Género: {resultados?.Genre}</p>
+        <p>Director: {resultados?.Director}</p>
+        <p>Actores principales: {resultados?.Actors}</p>
+        <p>Sinopsis: {resultados?.Plot}</p>
+        <p>Duración: {resultados?.Runtime}</p>
+        <p>Idioma: {resultados?.Language}</p>
+        <p>País: {resultados?.Country}</p>
+        <p>Puntaje IMDb: {resultados?.imdbRating}</p>
       </div>
     </>
   )
