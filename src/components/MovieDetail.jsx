858c00fe6fe95f5d79c61bd012c3870b217877d9
VICTORIA JOANA KRIGER CONTI <49439065@est.ort.edu.ar>
2026-05-05 08:45:47 -0300

    css y todo

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -4,17 +4,19 @@ import api from '../api';
 const MovieDetail = ({ movie }) => {
   const [errore, setErrore] = useState(false);
   const [resultados, setResultados] = useState(null);
-  useEffect(async () => {    
+  useEffect(() => {
+    const fetchMovie = async () => {
       try {
         setErrore(false);
+
         const response = await api.get("/", {
           params: {
             apikey: "8fb39e07",
             i: movie.imdbID
           }
         });
-        console.log(response.data)
-        if (response.data === null) {
+
+        if (!response.data) {
           setErrore(true);
           setResultados(null);
         } else {
@@ -23,28 +25,40 @@ const MovieDetail = ({ movie }) => {
 
       } catch (err) {
         setErrore(true);
-      }    
-  }, [])
+      }
+    };
 
+    if (movie) {
+      fetchMovie();
+    }
+  }, [movie]);
+
+  if (!resultados && !errore) return <p>Cargando...</p>;
+  if (errore) return <p>Error al cargar datos</p>;
 
   return (
     <>
       <div className="card-detalles">
-        <img
-          src={resultados?.Poster !== "N/A" ? resultados?.Poster : "https://via.placeholder.com/150"}
-          alt={resultados?.Title}
-        />
-        <p>Título: {resultados?.Title}</p>
-        <p>Año: {resultados?.Year}</p>
-        <p>Tipo: {resultados?.Type}</p>
-        <p>Género: {resultados?.Genre}</p>
-        <p>Director: {resultados?.Director}</p>
-        <p>Actores principales: {resultados?.Actors}</p>
-        <p>Sinopsis: {resultados?.Plot}</p>
-        <p>Duración: {resultados?.Runtime}</p>
-        <p>Idioma: {resultados?.Language}</p>
-        <p>País: {resultados?.Country}</p>
-        <p>Puntaje IMDb: {resultados?.imdbRating}</p>
+        <div className='DetallesImg'>
+          <img
+            src={resultados?.Poster !== "N/A" ? resultados?.Poster : "https://via.placeholder.com/150"}
+            alt={resultados?.Title}
+          />
+        </div>
+        <div className='DetallesInfo'>
+          <p>Título: {resultados?.Title}</p>
+          <p>Año: {resultados?.Year}</p>
+          <p>Tipo: {resultados?.Type}</p>
+          <p>Género: {resultados?.Genre}</p>
+          <p>Director: {resultados?.Director}</p>
+          <p>Actores principales: {resultados?.Actors}</p>
+          <p>Sinopsis: {resultados?.Plot}</p>
+          <p>Duración: {resultados?.Runtime}</p>
+          <p>Idioma: {resultados?.Language}</p>
+          <p>País: {resultados?.Country}</p>
+          <p>Puntaje IMDb: {resultados?.imdbRating}</p>
+        </div>
+
       </div>
     </>
   )
